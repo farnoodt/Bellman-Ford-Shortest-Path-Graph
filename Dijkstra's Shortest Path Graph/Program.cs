a15a90ec58c4b775bbe482dacf50084e342ee36b
Farnoud Taghdissi Borhani <Farnood_t@yahoo.com>
2021-08-15 00:13:41 -0700

    removed extra files

--- Dijkstra's Shortest Path Graph/Program.cs
+++ Dijkstra's Shortest Path Graph/Program.cs
@@ -6,7 +6,7 @@ namespace Dijkstra_s_Shortest_Path_Graph
     {
         static void Main(string[] args)
         {
-            Graph1 g = new Graph1(6);
+            Graph g = new Graph(6);
             g.AddEdge(1, 2, 1);
             g.AddEdge(2, 3, 7);
             g.AddEdge(2, 4, -2);
